f8becdcef5408c257a6db36f102d0eab8b0574fa
Pat Cody <patcody028@gmail.com>
2020-08-17 23:38:05 -0400

    Add intial version of the program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gif-blaster
 Blast your friends with GIFs
+
+Thanks to this stack overflow [post](https://stackoverflow.com/questions/11812184/how-to-send-an-imessage-text-with-applescript-only-in-provided-service) for the AppleScript!
---
+++ gif-blaster.py
@@ -0,0 +1,32 @@
+import requests
+from dotenv import load_dotenv
+import os
+import random
+import time
+load_dotenv()
+
+categories = ['dog', 'cat', 'bird', 'funny']
+phoneNumber = os.getenv("PHONE_NUMBER")
+
+URL="http://api.giphy.com/v1/gifs/random"
+randCategory = random.randint(0, len(categories) - 1)
+PARAMS = {'api_key': os.getenv("API_KEY"), 'tag': categories[randCategory]}
+
+startTime = time.time()
+runTime = 25200 # 7 hours in seconds
+waitTime = 1800 # 30 minutes in seconds
+
+while (time.time() - startTime) < runTime:
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    gifLink = data["data"]["url"]
+
+    print("Sending", categories[randCategory], "GIF:", gifLink)
+
+    command = 'osascript sendMessage.scpt ' + phoneNumber + ' ' + gifLink
+
+    os.system(command)
+    time.sleep(waitTime)
+
+print("Finished!")
+
