b687f1068498ee8a1ca4860d13f6718c5b7e5d62
Pat Cody <patcody028@gmail.com>
2020-08-18 07:34:43 -0400

    Fix category selection occurring only once

--- gif-blaster.py
+++ gif-blaster.py
@@ -9,14 +9,14 @@ categories = ['dog', 'cat', 'bird', 'funny']
 phoneNumber = os.getenv("PHONE_NUMBER")
 
 URL="http://api.giphy.com/v1/gifs/random"
-randCategory = random.randint(0, len(categories) - 1)
-PARAMS = {'api_key': os.getenv("API_KEY"), 'tag': categories[randCategory]}
 
 startTime = time.time()
 runTime = 25200 # 7 hours in seconds
 waitTime = 1800 # 30 minutes in seconds
 
 while (time.time() - startTime) < runTime:
+    randCategory = random.randint(0, len(categories) - 1)
+    PARAMS = {'api_key': os.getenv("API_KEY"), 'tag': categories[randCategory]}
     r = requests.get(url=URL, params=PARAMS)
     data = r.json()
     gifLink = data["data"]["url"]
